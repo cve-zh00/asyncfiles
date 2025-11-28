eb1c73c7e1e2dfd5c87f0beb8a63b1225fd83ea3
cve-zh00 <bastiang@uc.cl>
2025-11-28 12:31:57 -0300

    deploy: add pipeline to deploy

--- asyncfiles/__init__.py
+++ asyncfiles/__init__.py
@@ -8,6 +8,11 @@ from .files import BinaryFile, TextFile
 from .types import OpenTextMode
 from .utils import FileMode, mode_to_posix
 
+__version__ = "0.1.1"
+__author__ = "Bastián García"
+__email__ = "bastiang@uc.cl"
+__all__ = ["open", "__version__"]
+
 
 def get_loop(
     loop: Optional[asyncio.AbstractEventLoop] = None,
