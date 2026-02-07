5b44b64a69bd8a3a14d19f511ebd5d36a4493314
semantic-release <semantic-release>
2026-02-07 00:55:32 +0000

    1.4.0
    Automatically generated by python-semantic-release

--- asyncfiles/__init__.py
+++ asyncfiles/__init__.py
@@ -9,7 +9,7 @@ from .types import OpenTextMode
 from .utils import FileMode, mode_to_posix
 
 
-__version__ = "1.3.0"
+__version__ = "1.4.0"
 __author__ = "Bastián García"
 __email__ = "bastiang@uc.cl"
 __all__ = ["open", "__version__"]
